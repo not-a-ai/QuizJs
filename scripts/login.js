e7a1f66ba78c00aba32f14490736b30ab6c738cc
EdmarAntunes <edmarufmg@gmail.com>
2024-03-15 20:51:51 -0300

    New commit

--- scripts/login.js
+++ scripts/login.js
@@ -35,7 +35,7 @@ function onClickLogin() {
     document.getElementById("session_pw").value = "";
     if (checkLogin(userName, passwordText)) {
         // Abre a página inicial do quiz
-        window.open("quiz-home.html", '_self');
+        window.open("trilhas.html", '_self');
     } else {
         // Exibe mensagem de erro
         document.getElementById("Warning-Frame").classList.remove("hidden");
